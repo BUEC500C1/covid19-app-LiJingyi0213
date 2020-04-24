cda1757773d972aea9adaa8e82b751d806044169
Jingyi Li <55245473+LiJingyi0213@users.noreply.github.com>
2020-04-24 16:21:48 -0400

    Update App.js

--- step3/App.js
+++ step3/App.js
@@ -1,112 +1,21 @@
-import React, { Component } from 'react'
-import {StyleSheet,TouchableOpacity,Text,View,} from 'react-native'
-import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
-
-export default class Map extends Component {
-  constructor(props){
-    super(props)
-    this.state = {countryname:'',totalCases: '',totalRecovered: '',totalDeaths: '',
-      jsondata:[]
-    };
-}
-  componentDidMount() {
-    var requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch('https://api.covid19api.com/summary', requestOptions)
-      .then(response => response.json())
-      .then(json => {
-        this.setState({
-          totalCases: json['Countries'][236]['TotalConfirmed'],
-        });
-        this.setState({
-          totalRecovered: json['Countries'][236]['TotalRecovered'],
-        });        
-        this.setState({
-          totalDeaths: json['Countries'][236]['TotalDeaths'],
-        });
-        this.setState({
-          countryname: json['Countries'][236]['Country'],
-        });
-       
-        this.setState({ 
-          jsondata: json.Global,
-        });
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
+  
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+export default
+class App extends React.Component {
   render() {
     return (
-
-      <View>
-        <MapView
-          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-          style={styles.map}
-          region={{
-            
-            latitude: 37,
-            longitude: -100,
-            latitudeDelta: 35,
-            longitudeDelta: 35,
-            
-          }}
-        >
-          <Marker
-          coordinate = {{latitude: 40, longitude: -100}}
-          >
-            <Callout>
-              <Text style = {styles.text}>{this.state.countryname}</Text>
-              <Text style = {styles.text}>Totoal Confirmed:{this.state.totalCases}</Text>
-              <Text style = {styles.text}>Totoal Recovered:{this.state.totalRecovered}</Text>
-              <Text style = {styles.text}>Totoal Deaths:{this.state.totalDeaths}</Text>
-            </Callout>
-          </Marker>
-          <Marker
-          coordinate = {{latitude: 35.8, longitude: 104}}
-          >
-            <Callout>
-              <Text style = {styles.text}>China</Text>
-              <Text style = {styles.text}>Totoal Confirmed:{this.state.totalCNCases}</Text>
-              <Text style = {styles.text}>Totoal Recovered:{this.state.totalCNRecovered}</Text>
-              <Text style = {styles.text}>Totoal Deaths:{this.state.totalCNDeaths}</Text>
-            </Callout>
-          </Marker>
-
-        </MapView>
-      </View>
-    )
+      <MapView
+         style={{ flex: 1 }}
+         provider={PROVIDER_GOOGLE}
+         initialRegion={{
+         latitude: 42.347870,
+         longitude: -71.117089,
+         latitudeDelta: 0.0922,
+         longitudeDelta: 0.0421}}
+         showsUserLocation
+      />
+    );
   }
-};
-
-const styles = StyleSheet.create({
-  popup:{
-    backgroundColor: '#233',
-  },
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent:'center'
-  },
-  map: {
-     height: '100%',
-     width: '100%'
-  },
-  title: {
-    color: '#223',
-    padding:20,
-    fontWeight: 'bold',
-    fontSize: 30,
-    textAlign: 'center'
-  },
-  text: {
-    color: '#000',
-    padding: 2,
-    fontSize:15,
-    textAlign: 'center'
-  },
-});
-  
+}
